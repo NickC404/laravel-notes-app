42a60c6bce18634e0dba3765bfe511bf4ef1766a
Nick Cotterill <hello@nickcotterill.co.uk>
2025-08-01 20:09:36 +0100

    - Install Laravel Sanctum - Add Controller, Model, Route and migration for Notes

---
+++ app/Http/Controllers/Api/NoteController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Note;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json(Note::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+//        $request->validate([
+//            'title' => 'required|string|max:255',
+//            'content' => 'required|text|max:65535',
+//        ]);
+
+        $note = Note::create([
+            'title' => 'Test Note Title',
+            'content' => 'Test Note Content',
+        ]);
+        $note->save();
+        return response()->json($note);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): JsonResponse
+    {
+        $note = Note::findOrFail($id);
+
+        return response()->json($note);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): JsonResponse
+    {
+        $note = Note::findOrFail($id);
+
+        $note->update($request->all());
+        $note->save();
+        return response()->json($note);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
