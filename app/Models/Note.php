e22337cfbaf3b623ec190153c99260c6e0cf000a
Nick Cotterill <18194821+NickC404@users.noreply.github.com>
2025-08-04 21:26:20 +0100

    Adds missing return type

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Note extends Model
@@ -13,7 +14,7 @@ class Note extends Model
     use HasTimestamps;
     protected $fillable = ['title', 'content', 'user_id'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
